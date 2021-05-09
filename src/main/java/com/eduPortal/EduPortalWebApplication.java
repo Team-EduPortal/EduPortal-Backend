8712f6b690bbdbe11fe726cbc78247d21a69e028
krishna-saurav <krishna.lm99@gmail.com>
2021-05-09 22:21:21 +0530

    moving to root folder

---
+++ src/main/java/com/eduPortal/EduPortalWebApplication.java
@@ -0,0 +1,20 @@
+package com.eduPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPortalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduPortalWebApplication.class, args);
+	}
+
+}
+
+/*
+<dependency>
+	<groupId>org.springframework.boot</groupId>
+	<artifactId>spring-boot-starter-data-jpa</artifactId>
+</dependency>
+*/
---
+++ src/main/java/com/eduPortal/Entity/MessageBody.java
@@ -0,0 +1,18 @@
+package com.eduPortal.Entity;
+
+public class MessageBody {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public MessageBody(String message){
+		this.message = message;
+	}
+}
